bf0899bb34884bc6f95949b4340218d6a51c2f4f
MaxPlay <maxplayyt@gmail.com>
2015-01-19 14:07:49 +0100

    BaseDesign

--- index.php
+++ index.php
@@ -1,7 +1,40 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/base.css">
+		<script src="js/jquery-1.11.2.min.js"></script>
+		
 	</head>
 	<body>
+		<header>
+			<div class="content">
+			
+			</div>
+		</header>
+		<div id="wrapper">
+			<nav>
+				<div class="user">
+				
+					<div class="user_info">
+						<div class="username">Username</div>
+					</div>
+				
+					<form>
+						<div role="textbox" class="post_text" maxlength="1000" contenteditable="true" aria-multiline="true" spellcheck="true" dir="ltr" aria-autocomplete="list" aria-expanded="false"></div>
+						<button class="post_button">Post</button>
+					</form>
+				</div>
+				<div class="content">
+				</div>
+				<footer>
+				<a href="">AGB</a> | <a href="">Kontakt</a> | <a href="">Impressum</a>
+				</footer>
+			</nav>
+			<div class="timeline">
+				<div class="timelineelement"></div>
+				<div class="timelineelement timelineend"><div class="end">Nothing more to load.</div></div>
+			</div>
+		</div>
 	</body>
 </html>
